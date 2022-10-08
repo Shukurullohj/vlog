0c52c0292c4334e3ab6102ca726a4755853b1898
Shukurulloh <58078877+Shukurullohj@users.noreply.github.com>
2022-10-08 09:27:54 +0500

    Delete bootstrap.php

--- bootstrap.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$connect = mysqli_connect('ec2-3-229-165-146.compute-1.amazonaws.com', 'cxvpnibvhvosfz', '644c4166ab16d69bfd5f6f19a1271d6877961352be0fcb3d57e38fc35f8a7b70''dca2agsru7q048') or die ("database connection error");
