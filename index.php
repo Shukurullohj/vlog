d3bb71fce867828852e8879432bd46fd91bc476a
Shukurulloh <58078877+Shukurullohj@users.noreply.github.com>
2022-10-08 09:27:39 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
 
-require 'bootstrap.php';
-
-
-$tasks = $connect->query(SELECT FROM * todos);
 
 <h1>My Tasks</h1>
 
-<?php foreach ($tasks as $task) : ?>
-    <li>
-        <?php if ($task->completed) : ?>
-            <strike><?= $task->description; ?></strike>
-        <?php else : ?>
-            <?= $task->description; ?>
-        <?php endif; ?>
-    </li>
-<?php endforeach; ?>
